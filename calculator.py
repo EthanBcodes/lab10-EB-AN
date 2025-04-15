f557d756b0db6b3b7939a4e022db482422db2c41
Ethan Bond <ethanbond2007@gmail.com>
2025-04-14 20:58:46 -0400

    Restored calculator.py

---
+++ calculator.py
@@ -0,0 +1,35 @@
+# https://github.com/EthanBcodes/lab10-EB-AN
+# Partner 1: Ethan Bond
+# Partner 2: An Ngo
+
+import math
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of a negative number")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+def logarithm(a, b):
+    if a <= 0 or b <= 0 or a == 1:
+        raise ValueError("Invalid input for logarithm")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
