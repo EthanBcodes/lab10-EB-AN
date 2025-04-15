8a441d3004db962c187c22cf5f3dbfffec755476
nadidwin100 <anngo191@yahoo.com>
2025-04-14 20:47:53 -0400

    test calculator

--- test_calculator.py
+++ test_calculator.py
@@ -2,13 +2,15 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-2, 5), 3)
+        self.assertEqual(add(0, 7), 7)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(3, 5), -2)
+        self.assertEqual(subtract(7, 0), 7)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -18,20 +20,19 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            div(10, 0)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self):
+        self.assertEqual(log(2, 8), 3)
+        self.assertEqual(log(10, 1), 0)
+        with self.assertRaises(ValueError):
+            log(10,-5)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            log(1,10)
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
@@ -51,6 +52,6 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     ##########################
 
-# Do not touch this
+
 if __name__ == "__main__":
     unittest.main()
