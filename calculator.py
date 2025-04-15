ec31e824d4b494e0b2c49db60cc9c56b549642bd
Ethan Bond <ethanbond2007@gmail.com>
2025-04-14 20:30:48 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -1,35 +1,6 @@
-
-import math
-
-def add(a, b): 
-    return a + b
-
-def sub(a, b):
-    return a - b
-
-def mul(a, b):
-    return a * b
-
-def div(a, b):
-    try:
-        if a == 0:
-            raise ZeroDivisionError("Cannot divide by zero")
-        return b / a
-    except ZeroDivisionError as e:
-        print(f"Caught an error: {e}")
-
-def log(a, b):
-    try:
-        if a <= 0 or a == 1:
-            raise ValueError("Base 'a' must be greater than 0 and not equal to 1.")
-        if b <= 0:
-            raise ValueError("Argument 'b' must be greater than 0.")
-        return math.log(b,a)
-    except ValueError as e:
-        print(f"ValueError: {e}")
-
-def exp(a, b):
-    return a ** b
+# https://github.com/EthanBcodes/lab10-EB-AN
+# Partner 1: Ethan Bond
+# Partner 2: [An Ngo]
 
 import math
 
@@ -50,6 +21,11 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
 def logarithm(a, b):
     if a <= 0 or b <= 0 or a == 1:
         raise ValueError("Invalid input for logarithm")
@@ -57,4 +33,3 @@ def logarithm(a, b):
 
 def exponent(a, b):
     return a ** b
-
